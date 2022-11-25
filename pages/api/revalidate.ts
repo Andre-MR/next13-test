046d27e6fcecec4aed33fe602b8cddaa71a4c479
Andre-MR <andreinternet@gmail.com>
2022-11-25 00:48:24 -0300

    basic revalidate api

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+) {
+  try {
+    await res.revalidate("/");
+    return res.json("revalidated: true");
+  } catch (err) {
+    return res.status(500).send("Error revalidating");
+  }
+}
